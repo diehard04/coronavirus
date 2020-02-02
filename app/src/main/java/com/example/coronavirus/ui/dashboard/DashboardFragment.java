5c485c73918a6d4e5cc92cac41f4139a3a3af39c
DIPAK KUMAR MEHTA <dipakmehta0910@gmail.com>
2020-02-02 22:33:38 +0530

    first

---
+++ app/src/main/java/com/example/coronavirus/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,39 @@
+package com.example.coronavirus.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.coronavirus.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+    private WebView webView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        webView = root.findViewById(R.id.webview);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        webView.loadUrl("https://www.journaldev.com");
+        return root;
+    }
+}
