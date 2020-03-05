be60fffedc6695e0d8af8d54bd6c799e553095a4
Dipak Kumar Mehta <dipakm@invendis.com>
2020-03-05 10:16:01 +0530

    error

--- app/src/main/java/com/diehard04/coronavirus/ui/world/WorldFragment.java
+++ app/src/main/java/com/diehard04/coronavirus/ui/world/WorldFragment.java
@@ -19,8 +19,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.diehard04.coronavirus.R;
 import com.diehard04.coronavirus.adapters.CountriesAdapter;
 import com.diehard04.coronavirus.model.CountriesModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +36,6 @@ public class WorldFragment extends Fragment {
     private CountriesAdapter mCountriesAdapter;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mReferenceCountriesModel;
-
     private List<CountriesModel> countriesModelList = new ArrayList<>();
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         worldViewModel =
@@ -45,7 +47,26 @@ public class WorldFragment extends Fragment {
     }
 
     private void firebaseDatabaseHelper() {
+        countriesModelList.clear();
         mDatabase = FirebaseDatabase.getInstance();
+        mReferenceCountriesModel = mDatabase.getReference("world");
+        mReferenceCountriesModel.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                System.out.println("onDataChange= " + dataSnapshot.getValue());
+//                List<String> keys = new ArrayList<>();
+//                for (DataSnapshot keyMode : dataSnapshot.getChildren()) {
+//                    keys.add(keyMode.getKey());
+//                    CountriesModel countriesModel = keyMode.getValue(CountriesModel.class);
+//                    countriesModelList.add(countriesModel);
+//                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("databaseError= " + databaseError.getDetails());
+            }
+        });
     }
 
     private void initView(View root) {
