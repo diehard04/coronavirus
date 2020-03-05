1c2ac38cb4da38bd300802e508880821cd2c07fb
Dipak Kumar Mehta <dipakm@invendis.com>
2020-03-05 18:07:26 +0530

    india adapter

--- app/src/main/java/com/diehard04/coronavirus/ui/india/IndiaFragment.java
+++ app/src/main/java/com/diehard04/coronavirus/ui/india/IndiaFragment.java
@@ -11,25 +11,26 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.diehard04.coronavirus.R;
 
 public class IndiaFragment extends Fragment {
 
     private IndiaViewModel indiaViewModel;
+    private RecyclerView fIndiaRvCities;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         indiaViewModel =
                 ViewModelProviders.of(this).get(IndiaViewModel.class);
         View root = inflater.inflate(R.layout.fragment_india, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        indiaViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        initViews(root);
         return root;
+    }
+
+    private void initViews(View root) {
+        fIndiaRvCities = root.findViewById(R.id.f_india_rv_cities);
+
     }
 }
